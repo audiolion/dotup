3bbbfa372474f32ab00035a19f54c6041ede8a82
Ryan Castner <castner.rr@gmail.com>
2019-02-16 14:07:14 -0500

    feat: Black formatting

--- dotup.py
+++ dotup.py
@@ -22,8 +22,11 @@ def update_symlink(filename, force=None):
             return True
     return False
 
+
 @click.command('dotup')
-@click.option('--force', '-f', default=False, help="Overwrite existing symlinks.", type=bool)
+@click.option(
+    '--force', '-f', default=False, help="Overwrite existing symlinks.", type=bool
+)
 def dotup(force):
     non_dotfiles = []
     home = str(Path.home())
@@ -36,14 +39,20 @@ def dotup(force):
         if success:
             print(f'Symlinked {crayons.red(filename)}@ -> {home}/dotfiles/{filename}')
         else:
-            prompt_remove = input(f'File already exists at {crayons.yellow(f"{home}/{filename}")}, overwrite it? [y/n] ')
+            prompt_remove = input(
+                f'File already exists at {crayons.yellow(f"{home}/{filename}")}, overwrite it? [y/n] '
+            )
             if prompt_remove == 'y':
                 update_symlink(filename, True)
             else:
                 print(f'{crayons.magenta("Skipping")} {filename}')
 
     for filename in non_dotfiles:
-        print(f'\n{crayons.magenta("Skipped")} {crayons.yellow(f"{home}/{filename}")}, filename does not begin with \033[4m{crayons.cyan(".")}\033[0m')
+        print(
+            f'\n{crayons.magenta("Skipped")} {crayons.yellow(f"{home}/{filename}")},',
+            f'filename does not begin with \033[4m{crayons.cyan(".")}\033[0m',
+        )
+
 
 if __name__ == "__main__":
     dotup()
